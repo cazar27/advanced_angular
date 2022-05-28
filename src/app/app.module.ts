bc12fb8394e9c5f0669d4271a24ea1c5a36888bf
cazar27 <carloszr27@gmail.com>
2022-05-28 15:44:28 +0200

    add assets style without errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { MainPageComponent } from './not-found/main-page/main-page.component';
+import { DashboardComponent } from './content/dashboard/dashboard.component';
+import { NotFoundComponent } from './content/not-found/not-found.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainPageComponent,
+    DashboardComponent,
+    NotFoundComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
